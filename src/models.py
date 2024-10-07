2d433a155b58a4a8e134f664814960c4318a1d75
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-10-07 23:40:50 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -27,44 +27,46 @@ class Address(Base):
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)    
-    first_name   = Column(String(100)) 
-    last_name   = Column(String(150)) 
-    user_name   = Column(String(150)) 
-    password = Column(String(150))
-    email = Column(String(150))
-
-class Favorite(Base):
-    __tablename__ = 'favorite'
-    id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, ForeignKey('user.id'))
+    id = Column(Integer, primary_key=True)  
+    user_name   = Column(String(100), nullable=False)
+    password = Column(String(100), nullable=False)
 
 class Character(Base):
     __tablename__ = 'character'
     id = Column(Integer, primary_key=True)    
-    birth_year = Column(String(20))
-    eye_color = Column(String(50))
-    gender = Column(String(50))
-    hair_color  = Column(String(50))
-    height = Column(Integer)
-    mass   = Column(Integer) 
-    name   = Column(String(100)) 
-    skin_color = Column(String(50))
-    created = Column(String(80))
-    edited  = Column(String(80))
-    character_id = Column(Integer, ForeignKey('planet.id'))
+    birth_year = Column(String(20), nullable=True)
+    eye_color = Column(String(50), nullable=True)
+    gender = Column(String(50), nullable=True)
+    hair_color  = Column(String(50), nullable=True)
+    height = Column(String(50), nullable=True)
+    mass   = Column(String(50), nullable=True)
+    name   = Column(String(100), nullable=True) 
+    skin_color = Column(String(50), nullable=True)
+    created = Column(String(80), nullable=True)
+    edited  = Column(String(80), nullable=True)
+    
 
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True) 
-    name   = Column(String(100)) 
-    diameter = Column(Integer)
-    climate = Column(String(50))
+    name   = Column(String(100), nullable=True) 
+    diameter = Column(String(100), nullable=True) 
+    climate = Column(String(100), nullable=True) 
+    population   = Column(String(100), nullable=True) 
+    gravity = Column(String(100), nullable=True)
+    terrain = Column(String(100), nullable=True)
+    rotation_period = Column(String(100), nullable=True)
 
-
-
-
-
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    name   = Column(String(100), nullable=False) 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user =  relationship(User)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character =  relationship(Character)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet =  relationship(Planet)
 
     def to_dict(self):
         return {}
